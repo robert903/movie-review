b485e731174eaade261d39968054d853b777c24f
robert richards <richardsj896@yahoo.com>
2015-08-15 21:44:19 -0500

    review scaffold

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,5 @@
 class Movie < ActiveRecord::Base
   belongs_to :user
-  
   has_attached_file :image, styles: { medium: "300x400>"}
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
