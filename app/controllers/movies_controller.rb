87b91daac9de16517c42fae0c45632446d737475
robert richards <richardsj896@yahoo.com>
2015-08-17 00:22:46 -0500

    added logo, added more styling to forms,change navbar color

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,6 +5,12 @@ class MoviesController < ApplicationController
 
   def index
     @movies = Movie.all.order("created_at DESC")
+    
+    if @reviews.blank?
+      @avg_review = 0
+    else
+      @avg_review = @reviews.average(:rating).round(2)
+    end
   end
 
 
